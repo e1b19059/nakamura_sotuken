c9e5de15c8fd191165ee02fc15f6362c196fada8
e1b19059 <73092824+e1b19059@users.noreply.github.com>
2022-08-06 11:43:51 +0900

    不要な部分の削除

--- components/generator/generator.js
+++ components/generator/generator.js
@@ -1,14 +1,6 @@
 import * as Blockly from 'blockly/core';
 import 'blockly/javascript';
 
-/*Blockly.JavaScript['test_react_field'] = function () {
-    return 'console.log(\'custom block\');\n';
-};
-
-Blockly.JavaScript['test_react_date_field'] = function (block) {
-    return 'console.log(' + block.getField('DATE').getText() + ');\n';
-};*/
-
 Blockly.JavaScript['go_left'] = function () {
     return 'go_left();\n';
 };
